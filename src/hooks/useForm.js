13939b6a93da75461eb82b5f490f1d21cb5f2a50
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-02 15:50:14 -0300

    photoPost

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
     regex:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email valido!'
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize numero apenas'
   }
 }
 
@@ -15,7 +19,7 @@ const useForm = (type) => {
   function validate(value){
     if(type === false) return true
     if(value.length === 0) {
-      setError('Preencha uma valor.')
+      setError("Preencha esse campo")
       return false
     }else if (types[type] && !types[type].regex.test(value)){
       setError(types[type].message)
