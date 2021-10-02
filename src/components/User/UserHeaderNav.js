422592560d583c6e3280b42b2069346a6661e406
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-02 15:03:49 -0300

    menu mobile

--- src/components/User/UserHeaderNav.js
+++ src/components/User/UserHeaderNav.js
@@ -1,34 +1,53 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { UserContext } from '../../UserContext'
 import styles from './UserHeaderNav.module.css'
 import { ReactComponent as Feed } from '../../assets/feed.svg'
 import { ReactComponent as Stats } from '../../assets/estatisticas.svg'
 import { ReactComponent as Post } from '../../assets/adicionar.svg'
 import { ReactComponent as Exit } from '../../assets/sair.svg'
+import useMedia from '../../hooks/useMedia'
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null)
   const { userLogout } = React.useContext(UserContext)
+
+  const mobile = useMedia('(max-width: 40rem)')
+  const [mobileMenu, setMobileMenu] = React.useState(false)
+
+  const {pathname} = useLocation()
+
+  React.useEffect(() => {
+    setMobileMenu(false)
+  },[pathname])
+
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/user" end>
-        <Feed />
-        {mobile && 'Minhas fotos'}
-      </NavLink>
-      <NavLink to="/user/stats">
-        <Stats />
-        {mobile && 'Estatística'}
-      </NavLink>
-      <NavLink to="/user/photo-post">
-        <Post />
-        {mobile && 'Adicionar foto'}      
-      </NavLink>
-      <button onClick={userLogout}>
-        <Exit />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label="menu"
+          className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}>
+        <NavLink to="/user" end>
+          <Feed />
+          {mobile && 'Minhas fotos'}
+        </NavLink>
+        <NavLink to="/user/stats">
+          <Stats />
+          {mobile && 'Estatística'}
+        </NavLink>
+        <NavLink to="/user/photo-post">
+          <Post />
+          {mobile && 'Adicionar foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Exit />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   )
 }
 
