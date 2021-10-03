b8b36815fd8e2550ba141e1515ceb132c8ec8ac1
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-03 16:36:54 -0300

    user profile e page 404

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { UserStorage } from './UserContext'
 import User from './components/User/User'
 import ProtectedRouter from './components/Helper/ProtectedRouter'
 import Photo from './components/Photo/Photo'
+import UserProfile from './components/User/UserProfile'
+import NotFound from './components/NotFound'
 
 const App = () => {
   return (
@@ -20,7 +22,9 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
             <ProtectedRouter path="/user/*" element={<User />} />
+            <Route path="/profile/:user" element={<UserProfile />} />
             <Route path="/photo/:id" element={<Photo />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
