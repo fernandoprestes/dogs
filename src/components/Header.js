e9d3e088bd4c6aabffb3cec1ee951cd944064802
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-03 13:56:42 -0300

    deletar foto

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
           <Dogs />
         </Link>
         {data ? (
-          <Link className={styles.login} to="/conta">
+          <Link className={styles.login} to="/user">
             {data.username}
           </Link>
         ) : (
