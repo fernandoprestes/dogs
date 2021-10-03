8ab134df777717495d6bdb99030b2afa8c826bd9
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-03 13:40:06 -0300

    comments

--- src/api.js
+++ src/api.js
@@ -68,17 +68,31 @@ export function PHOTO_GET(id) {
     url: `${API_URL}/api/photo/${id}`,
     options: {
       method: 'GET',
-      cache: "no-store",
+      cache: 'no-store'
     }
   }
 }
 
-export function PHOTOS_GET({page, total, user}) {
+export function PHOTOS_GET({ page, total, user }) {
   return {
     url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
     options: {
       method: 'GET',
-      cache: "no-store",
+      cache: 'no-store'
+    }
+  }
+}
+
+export function COMMENT_POST(id, body) {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+      body: JSON.stringify(body)
     }
   }
 }
