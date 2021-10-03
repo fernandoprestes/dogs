6fd7126243e529c35aa29ba278bc81d42b0f9498
fernandoprestes <fernandoprestesc@gmail.com>
2021-10-03 15:29:47 -0300

    infinite scroll

--- src/components/Feed/FeedPhotos.js
+++ src/components/Feed/FeedPhotos.js
@@ -6,17 +6,18 @@ import Loading from '../Helper/Loading'
 import FeedPhotosItem from './FeedPhotosItem'
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({ user, page, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch()
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 })
-      const { json } = await request(url, options)
-      console.log(json.length)
+      const total = 3
+      const { url, options } = PHOTOS_GET({ page, total, user })
+      const { response, json } = await request(url, options)
+      if (response && response.ok && json.length < total) setInfinite(false)
     }
     fetchPhotos()
-  }, [request])
+  }, [request, user, page, setInfinite])
 
   if (error) return <Error error={error} />
   if (loading) return <Loading />
@@ -24,7 +25,11 @@ const FeedPhotos = ({setModalPhoto}) => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map(photo => (
-          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     )
